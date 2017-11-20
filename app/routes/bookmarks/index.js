54217acb006379b7fe525ce20928dca696a2ddc0
rnaka <watanabe.raphael@gmail.com>
2017-11-20 09:03:17 -0500

    Routes

---
+++ app/routes/bookmarks/index.js
@@ -0,0 +1,10 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+    model() {
+        return this.get('store').findAll('bookmark');
+    },
+    renderTemplate() {
+        this.render('about');
+    }
+});
